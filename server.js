e9acf1be2dda038800b44678a13b9334aade43a8
Andrea Spadaccini <andrea.spadaccini@dieei.unict.it>
2015-03-31 22:50:57 +0000

    Remove useless async package

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var path = require('path');
 
-var async = require('async');
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
