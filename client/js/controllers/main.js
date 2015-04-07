c672dd1f7cb212bd100684409ad530e75a55cacd
Antonino Parisi <tabman83@gmail.com>
2015-04-07 07:54:40 +0000

    Bugfix for countdown; logging removed.

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -10,7 +10,6 @@
     angular.module('DublinBusTrackerApp').controller('MainController', ['$scope', '$interval', '$rootScope', function($scope, $interval, $rootScope) {
         
         $scope.$on('socket:bus', function (event, data) {
-          console.log(data);
           if (data.length > 0) {
             $scope.nextBus = data.slice(0,1).pop();
             $scope.otherBuses = data.slice(1);
@@ -25,11 +24,7 @@
           
           if(!$scope.nextBus) return;
           
-          console.log(moment.utc($scope.nextBus.expectedTime).diff(moment.utc(), 'minutes'));
-          console.log(moment.utc($scope.nextBus.expectedTime).format());
-          console.log(moment.utc().format());
-
-          if( moment($scope.nextBus.expectedTime).diff(moment.utc(), 'minutes') <= $scope.$storage.warningTime ) {
+          if( moment($scope.nextBus.expectedTime).diff(moment.utc(), 'minutes') < $scope.$storage.warningTime ) {
             $rootScope.isRunningOutOfTime = true;
           } else {
             $rootScope.isRunningOutOfTime = false;
