35f0b3b0c1f25e7877aae6ed74b6e99d79f6c7ea
Antonino Parisi <tabman83@gmail.com>
2015-04-07 09:26:13 +0000

    Added audio notification and setting.

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -7,8 +7,11 @@
 
 (function(angular, undefined) {
     'use strict';
-    angular.module('DublinBusTrackerApp').controller('MainController', ['$scope', '$interval', '$rootScope', function($scope, $interval, $rootScope) {
-        
+    angular.module('DublinBusTrackerApp').controller('MainController', ['$scope', '$interval', '$rootScope', 'ngAudio', function($scope, $interval, $rootScope, ngAudio) {
+      
+        var sound = ngAudio.load("sounds/klaxon.mp3");
+        //git sound.loop = true;
+
         $scope.$on('socket:bus', function (event, data) {
           if (data.length > 0) {
             $scope.nextBus = data.slice(0,1).pop();
@@ -26,8 +29,14 @@
           
           if( moment($scope.nextBus.expectedTime).diff(moment.utc(), 'minutes') < $scope.$storage.warningTime ) {
             $rootScope.isRunningOutOfTime = true;
+            if($scope.$storage.isAudioEnabled) {
+              sound.play();
+            }
           } else {
             $rootScope.isRunningOutOfTime = false;
+            if($scope.$storage.isAudioEnabled) {
+              sound.stop();
+            }
           }
         }, 1000);
         
